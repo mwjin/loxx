af93944769d26fc5b4aec3f9f8cc1a77081dcb45
Minwoo Jeong <jeongmwj@gmail.com>
2024-04-28 23:28:56 +0900

    Remove the `<print>` usage
    Not all compilers support this module.

--- lib/shape.cc
+++ lib/shape.cc
@@ -1,10 +1,5 @@
 #include "shape.h"
 
-#include <print>
-
 Rectangle::Rectangle(int width, int height) : width_{width}, height_{height} {}
 
-int Rectangle::GetSize() const {
-  std::print("width: {}, height: {}\n", width_, height_);
-  return width_ * height_;
-}
+int Rectangle::GetSize() const { return width_ * height_; }
