a76f1ce526ca36847a24872d3885231bfa9555ff
Minwoo Jeong <jeongmwj@gmail.com>
2024-05-19 11:22:15 +0900

    Update functions to read source code
    + Show the `>>` prefix in the prompt
    + Update `loxx::RunFile` to read all bytes from a file

--- lib/loxx.cc
+++ lib/loxx.cc
@@ -2,20 +2,32 @@
 
 #include <fstream>
 #include <iostream>
+#include <iterator>
 
 namespace loxx {
 void Run(const std::string& source) {
-  std::cout << "Run " << source << std::endl;
+  std::cout << "Get \"" << source << "\"" << std::endl;
 }
 }  // namespace loxx
 
 void loxx::RunFile(const std::string& file_path) {
   std::ifstream in_file{file_path};
-  std::string source{};
-  in_file >> source;
+  if (!in_file) {
+    std::cerr << "Can't open the file \"" << file_path << '\"' << std::endl;
+    return;
+  }
+
+  // Read all bytes and store them in a string
+  std::string source{std::istreambuf_iterator<char>(in_file),
+                     std::istreambuf_iterator<char>()};
   Run(source);
 }
 void loxx::RunPrompt() {
   std::string line{};
-  while (std::getline(std::cin, line)) Run(line);
+  for (;;) {
+    std::cout << ">> ";
+    auto& no_error = std::getline(std::cin, line);
+    if (!no_error) break;
+    Run(line);
+  }
 }
